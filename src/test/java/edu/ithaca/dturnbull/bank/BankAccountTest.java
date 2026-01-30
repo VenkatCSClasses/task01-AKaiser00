d2e07a9182a2c863f58d99073bce02b36ca09772
baneetpukhrambam <bpukhrambam@ithaca.edu>
2026-01-29 21:22:40 -0500

    Added comments to isEmailValidTest

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -3,7 +3,6 @@ package edu.ithaca.dturnbull.bank;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 class BankAccountTest {
 
     @Test
@@ -14,7 +13,7 @@ class BankAccountTest {
     }
 
     @Test
-    void withdrawTest() throws InsufficientFundsException{
+    void withdrawTest() throws InsufficientFundsException {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
 
@@ -23,38 +22,40 @@ class BankAccountTest {
     }
 
     @Test
-    void isEmailValidTest(){
+    void isEmailValidTest() {
         // Starting Tests
-        assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
-        assertFalse( BankAccount.isEmailValid(""));         // empty string
-
+        assertTrue(BankAccount.isEmailValid("a@b.com")); // boundary case: valid, minimal email address
+        assertFalse(BankAccount.isEmailValid("")); // boundary case: invalid, empty string
 
         // Testing Prefix
-        assertTrue(BankAccount.isEmailValid("abc-d@mail.com")); // valid, testing special char
-        assertTrue(BankAccount.isEmailValid("abc.def@mail.com")); // valid, testing period before @
-        assertTrue(BankAccount.isEmailValid("abc@mail.com")); // valid, normal email
-        assertTrue(BankAccount.isEmailValid("abc_def@mail.com")); // valid, testing special char
+        assertTrue(BankAccount.isEmailValid("abc-d@mail.com")); // not boundary: valid, testing special char
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.com")); // not boundary: valid, testing period before @
+        assertTrue(BankAccount.isEmailValid("abc@mail.com")); // not boundary: valid, normal email
+        assertTrue(BankAccount.isEmailValid("abc_def@mail.com")); // not boundary: valid, testing special char
 
-        assertFalse(BankAccount.isEmailValid("abc-@mail.com")); // special char at end of prefix
-        assertFalse(BankAccount.isEmailValid("abc..def@mail.com")); // special chars in a row
-        assertFalse(BankAccount.isEmailValid(".abc@mail.com")); // special char at beginning of prefix
-        assertFalse(BankAccount.isEmailValid("abc#def@mail.com")); // not permitted special char
-        assertFalse(BankAccount.isEmailValid("@mail.com")); // empty prefix
-        assertFalse(BankAccount.isEmailValid("abc@@mail.com")); // multiple @ symbols, in a row
-        assertFalse(BankAccount.isEmailValid("abc@mail@archive.com")); // multiple @ symbols
-
+        assertFalse(BankAccount.isEmailValid("abc-@mail.com")); // boundary: special char at end of prefix
+        assertFalse(BankAccount.isEmailValid("abc..def@mail.com")); // not boundary: invalid, special chars in a row
+        assertFalse(BankAccount.isEmailValid(".abc@mail.com")); // boundary: invalid, special char at beginning of
+                                                                // prefix
+        assertFalse(BankAccount.isEmailValid("abc#def@mail.com")); // not boundary: invalid, not permitted special char
+        assertFalse(BankAccount.isEmailValid("@mail.com")); // boundary: invalid, empty prefix
+        assertFalse(BankAccount.isEmailValid("abc@@mail.com")); // boundary: multiple @ symbols, in a row
+        assertFalse(BankAccount.isEmailValid("abc@mail@archive.com")); // not boundary: multiple @ symbols
 
         // Testing Domain
-        assertTrue(BankAccount.isEmailValid("abc.def@mail.cc")); // two character end domain
-        assertTrue(BankAccount.isEmailValid("abc.def@mail-archive.com")); // permitted special chars
-        assertTrue(BankAccount.isEmailValid("abc.def@mail.org")); // .org domain
-        assertTrue(BankAccount.isEmailValid("abc.def@mail.com")); // .com domain
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.cc")); // boundary: valid, two character end domain
+        assertTrue(BankAccount.isEmailValid("abc.def@mail-archive.com")); // not boundary: valid, permitted special
+                                                                          // chars
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.org")); // not boundary: valid, .org domain
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.com")); // not boundary: valid, .com domain
 
-        assertFalse(BankAccount.isEmailValid("abc.def@mail.c")); // one character end domain
-        assertFalse(BankAccount.isEmailValid("abc.def@mail#archive.com")); // not permitted special char
-        assertFalse(BankAccount.isEmailValid("abc.def@mail")); // non-existent domain
-        assertFalse(BankAccount.isEmailValid("abc.def@mail..com")); // multiple special chars in a row
-        assertFalse(BankAccount.isEmailValid("abc@")); // empty domain
+        assertFalse(BankAccount.isEmailValid("abc.def@mail.c")); // boundary: invalid, one character end domain
+        assertFalse(BankAccount.isEmailValid("abc.def@mail#archive.com")); // not boundary: invalid, not permitted
+                                                                           // special char
+        assertFalse(BankAccount.isEmailValid("abc.def@mail")); // boundary: invalid, non-existent domain tag
+        assertFalse(BankAccount.isEmailValid("abc.def@mail..com")); // not boundary: invalid, multiple special chars in
+                                                                    // a row
+        assertFalse(BankAccount.isEmailValid("abc@")); // boundary: invalid, empty domain
     }
 
     @Test
@@ -63,8 +64,8 @@ class BankAccountTest {
 
         assertEquals("a@b.com", bankAccount.getEmail());
         assertEquals(200, bankAccount.getBalance(), 0.001);
-        //check for exception thrown correctly
-        assertThrows(IllegalArgumentException.class, ()-> new BankAccount("", 100));
+        // check for exception thrown correctly
+        assertThrows(IllegalArgumentException.class, () -> new BankAccount("", 100));
     }
 
 }
