7df7220fcc6c5ee59149457746fb3d7dcd75174f
baneetpukhrambam <bpukhrambam@ithaca.edu>
2026-01-29 19:18:57 -0500

    completed isEmailValid code

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -8,42 +8,72 @@ public class BankAccount {
     /**
      * @throws IllegalArgumentException if email is invalid
      */
-    public BankAccount(String email, double startingBalance){
-        if (isEmailValid(email)){
+    public BankAccount(String email, double startingBalance) {
+        if (isEmailValid(email)) {
             this.email = email;
             this.balance = startingBalance;
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
         }
     }
 
-    public double getBalance(){
+    public double getBalance() {
         return balance;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
 
     /**
-     * @post reduces the balance by amount if amount is non-negative and smaller than balance
+     * @post reduces the balance by amount if amount is non-negative and smaller
+     *       than balance
      */
-    public void withdraw (double amount) throws InsufficientFundsException{
-        if (amount <= balance){
+    public void withdraw(double amount) throws InsufficientFundsException {
+        if (amount <= balance) {
             balance -= amount;
-        }
-        else {
+        } else {
             throw new InsufficientFundsException("Not enough money");
         }
     }
 
-
-    public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1){
+    public static boolean isEmailValid(String email) {
+        if (email.indexOf('@') == -1) {
             return false;
         }
-        else {
+        if (email.isEmpty()) {
+            return false;
+        }
+        String[] p_and_d = email.split("@");
+        if (p_and_d.length != 2) {
+            return false;
+        }
+
+        String prefix = p_and_d[0];
+        String domain = p_and_d[1];
+        String[] domain_parts = domain.split("\\.");
+        String end_domain = domain_parts[domain_parts.length - 1];
+        if (prefix.isEmpty() || domain.isEmpty()) {
+            return false;
+        }
+        if (String.valueOf(prefix.charAt(0)).matches("[^a-zA-Z0-9]")) {
+            return false;
+        }
+        if (String.valueOf(prefix.charAt(prefix.length() - 1)).matches("[^a-zA-Z0-9]")) {
+            return false;
+        }
+        if (!prefix.matches("[A-Za-z0-9._-]+") || !domain.matches("[A-Za-z0-9.-]+")) {
+            return false;
+        }
+        if (prefix.matches(".*[._-]{2,}.*") || domain.matches(".*[._-]{2,}.*")) {
+            return false;
+        }
+        if (!domain.contains(".")) {
+            return false;
+        }
+        if (end_domain.length() < 2) {
+            return false;
+        } else {
             return true;
         }
     }
